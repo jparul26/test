0cd3dab6737213a5f891d88e00084d3d7472a29a
jparul26 <50869847+jparul26@users.noreply.github.com>
2020-05-18 00:15:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,5 +28,5 @@ require("./config/passport")(passport);
 
 app.use("/api/users", users);
 
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.PORT || 5000; 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
